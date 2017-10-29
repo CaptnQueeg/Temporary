dd3dc69e56de84d27206811dad7a6c72461363d5
CaptnQueeg <33198469+CaptnQueeg@users.noreply.github.com>
2017-10-29 14:16:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 'Esc' to escape ;)");           
+
+            ConsoleKeyInfo input;
+            do
+            {
+              input = Console.ReadKey();
+            } while (input.Key != ConsoleKey.Escape);
+            
+        }
+    }
+}
